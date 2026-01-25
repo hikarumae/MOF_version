c197c3909a95255379b12b498af52a17b437ace7
tamo <maetamorihikaru@gmail.com>
2026-01-25 15:23:57 +0900

    環境変数名の修正

--- ai_judge.py
+++ ai_judge.py
@@ -7,7 +7,7 @@ from openai import AzureOpenAI
 def get_judgment(text_head, text_tail, base64_image):
     client = AzureOpenAI(
         azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key=os.getenv("AZURE_OPENAI_KEY"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
         api_version="2024-02-15-preview"
     )
     
